3b98bb05d0c6d841367e94bf01afc5c2cadcb8b3
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-03-06 09:03:49 -0300

    atividade 2

---
+++ ativ2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nome, Carro e País</title>
+</head>
+<body>
+
+<?php
+    $nomes = array ("Clístenes, Alfredo, Jraffis, Paullynelly");
+    $carros = array ("Porsche GT3 992, Lamborghini Huracán STO, McLaren 720s N-Largo, Ferrari F8 Tributo");
+    $paises = array ("França - Paris, EUA - Washington DC, Canadá - Ottawa, Itália - Roma, Brasil - Brasília");
+
+    echo $nomes[0];
+    echo ("</br>");
+    echo $carros[0];
+    echo ("</br>");
+    echo $paises[0];
+?>
+    
+</body>
+</html>
